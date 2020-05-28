8a2cb81b9639a065a842ed3088324687df9649e8
Andrew Leader <aleader@microsoft.com>
2020-05-28 14:13:57 -0700

    Started adding UWP snackbar

--- InterfacesUWP/InterfacesUWP/AppWindows/NativeUwpAppWindow.cs
+++ InterfacesUWP/InterfacesUWP/AppWindows/NativeUwpAppWindow.cs
@@ -11,20 +11,27 @@ using Windows.UI.Core;
 using ToolsPortable;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml;
+using InterfacesUWP.Snackbar;
+using BareMvvm.Core.Snackbar;
 
 namespace InterfacesUWP.AppWindows
 {
     public class NativeUwpAppWindow : Grid, INativeAppWindow
     {
         private GenericViewModelPresenter _presenter;
+        private BareSnackbarPresenter _snackbarPresenter = new BareSnackbarPresenter();
         private SystemNavigationManagerEnhanced _navigationManager;
 
         public Window Window { get; private set; }
+
+        public BareSnackbarManager SnackbarManager => _snackbarPresenter.Manager;
 
         public NativeUwpAppWindow()
         {
             _presenter = new GenericViewModelPresenter();
             Children.Add(_presenter);
+
+            Children.Add(_snackbarPresenter);
 
             Window = Window.Current;
 
