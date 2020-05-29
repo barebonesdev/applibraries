d912664899dd3de07ae3026f3d2f01403770d301
Andrew Leader <followthatleader@outlook.com>
2020-05-29 10:45:35 -0700

    Android snackbar

--- InterfacesDroid/InterfacesDroid/ViewModelPresenters/GenericViewModelPresenter.cs
+++ InterfacesDroid/InterfacesDroid/ViewModelPresenters/GenericViewModelPresenter.cs
@@ -5,15 +5,17 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Hardware.Display;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using BareMvvm.Core.ViewModels;
+using InterfacesDroid.Views;
 
 namespace InterfacesDroid.ViewModelPresenters
 {
-    public class GenericViewModelPresenter : FrameLayout
+    public class GenericViewModelPresenter : FrameLayout, IGetSnackbarAnchorView
     {
         public GenericViewModelPresenter(Context context) : base(context)
         {
@@ -47,6 +49,47 @@ namespace InterfacesDroid.ViewModelPresenters
                 // Create and set new content
                 var view = ViewModelToViewConverter.Convert(this, ViewModel);
                 base.AddView(view);
+            }
+        }
+
+        public virtual View GetSnackbarAnchorView()
+        {
+            if (base.ChildCount > 0)
+            {
+                return GetSnackbarAnchorView(base.GetChildAt(0));
+            }
+
+            return null;
+        }
+
+        public static View GetSnackbarAnchorView(View childView)
+        {
+            if (childView is IGetSnackbarAnchorView getAnchorView)
+            {
+                return getAnchorView.GetSnackbarAnchorView();
+            }
+            else if (childView is ViewGroup viewGroup)
+            {
+                var children = viewGroup.GetAllChildren().ToArray();
+                foreach (var item in children.OfType<IGetSnackbarAnchorView>())
+                {
+                    return item.GetSnackbarAnchorView();
+                }
+
+                foreach (var item in children.Where(i => !(i is IGetSnackbarAnchorView)))
+                {
+                    var answer = GetSnackbarAnchorView(item);
+                    if (answer != null)
+                    {
+                        return answer;
+                    }
+                }
+
+                return null;
+            }
+            else
+            {
+                return null;
             }
         }
     }
